e41e6d0664c061245f18109c9b3cc9123524eb84
hqx <869049149@qq.com>
2018-03-29 17:20:05 +0800

    弹框

--- app/src/main/java/com/bestvike/androiddevelopmentartexploration/MainActivity.java
+++ app/src/main/java/com/bestvike/androiddevelopmentartexploration/MainActivity.java
@@ -1,14 +1,18 @@
 package com.bestvike.androiddevelopmentartexploration;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 import com.bestvike.androiddevelopmentartexploration.homePpage.HomePageAdapter;
 import com.example.beaselibrary.base.BaseActivity;
 import com.example.beaselibrary.base.BaseRecyclerAdapter;
+import com.example.beaselibrary.interfaces.DialogListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,7 +48,65 @@ public class MainActivity extends BaseActivity {
         adapter.setOnItemClickListener(new BaseRecyclerAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, int position) {
-                showToast(list.get(position));
+                String str  = list.get(position);
+                showToast(str);
+                switch (position){
+                    case 0:
+                        showDialog(str);
+                        break;
+                    case 1:
+                        showDialog(str, new DialogListener() {
+                            @Override
+                            public void dialogCallBack(final AlertDialog dialog, TextView left, TextView right) {
+                                left.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                right.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                            }
+                        });
+                        break;
+                    case 2:
+                        showDialog(str, false, new DialogListener() {
+                            @Override
+                            public void dialogCallBack(final AlertDialog dialog, TextView left, TextView right) {
+                                left.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                right.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        dialog.dismiss();
+                                    }
+                                });
+                            }
+                        });
+                        break;
+                    case 3:
+                        showProgress(true,"请稍后。。。");
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                showProgress(false);
+                            }
+                        },2000);
+                        break;
+                    case 4:
+
+                        break;
+                }
             }
         });
 
--- beaselibrary/src/main/java/com/example/beaselibrary/base/BaseActivity.java
+++ beaselibrary/src/main/java/com/example/beaselibrary/base/BaseActivity.java
@@ -1,10 +1,15 @@
 package com.example.beaselibrary.base;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.beaselibrary.interfaces.DialogListener;
+import com.example.beaselibrary.util.ShowDialog;
 
 /**
  * Created by Administrator on 2018/3/29.
@@ -31,5 +36,59 @@ public abstract class BaseActivity extends Activity implements View.OnClickListe
         }
         mToast.setText(str);
         mToast.show();
+    }
+
+    /**
+     * 只有一个按钮的提示框
+     * @param str
+     */
+    public void showDialog(String str){
+        ShowDialog.getInstance().showDialog(this, str, true, new DialogListener() {
+            @Override
+            public void dialogCallBack(final AlertDialog dialog, TextView left, TextView right) {
+                left.setVisibility(View.GONE);
+                right.setText("知道了");
+                right.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * 含有两个按钮的提示框
+     * @param str
+     * @param listener
+     */
+    public void showDialog(String str , DialogListener listener){
+        ShowDialog.getInstance().showDialog(this,str,true,listener);
+    }
+
+    /**
+     * 两个按钮的提示
+     * @param str
+     * @param isTitle  title 是否展示 。f 不含有title , t 含有title
+     * @param listener
+     */
+    public void showDialog(String str , boolean isTitle , DialogListener listener){
+        ShowDialog.getInstance().showDialog(this,str,isTitle,listener);
+    }
+    /**
+     * 菊花
+     */
+    public void showProgress(boolean flag){
+        ShowDialog.getInstance().showProgress(flag,"",this);
+    }
+
+    /**
+     * 含有提示语的菊花
+     * @param flag
+     * @param str
+     */
+    public void showProgress(boolean flag,String str){
+        ShowDialog.getInstance().showProgress(flag,str,this);
     }
 }
